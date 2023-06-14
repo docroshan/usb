e355c8b45f6edc9548894707003b4daebc34e8e9
docroshan <roshanzameerm001@gmail.com>
2023-06-14 17:34:33 +0530

    sc

--- usb_run.py
+++ usb_run.py
@@ -26,20 +26,19 @@ try:
         create_config('TEST_CASES', 'pick_test', value, user_config_file)
         subprocess.run(['python', f'Testcases/{value}.py'], check=True)
 
+        # To get all the paths of the log file in result dir
+        path = Path('./results/')
+        win_paths = ['./'+str(i).split('\\')[-1] for i in path.iterdir() if i.suffix == '.log' and i.is_file()]
+
+        # Merging all available log files into a single log
+        with open('output.log', 'w') as output_file:
+            for path in win_paths:
+                if path.split('/')[-1].split('.')[0] in list_:
+                    content = open(path, 'r').read()
+                    output_file.write(content)
+
 except subprocess.CalledProcessError:
     print('Unable to run File or File Not Found')
-
-# To get all the paths of the log file in result dir
-path = Path('./results/')
-win_paths = ['./'+str(i).split('\\')[-1] for i in path.iterdir() if i.suffix == '.log' and i.is_file()]
-
-
-# Merging all available log files into a single log
-with open('output.log', 'w') as output_file:
-    for path in win_paths:
-        if path.split('/')[-1].split('.')[0] in list_:
-            content = open(path, 'r').read()
-            output_file.write(content)
 
 end = time.time()
 print(f"Total time taken:{str(round((end - start), 2))} sec")
